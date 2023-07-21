9e8b933e202644335abbfe69dd6425663c1b4e10
Altbeg <shmuelbisberg.acc@gmail.com>
2023-07-21 10:55:18 +0300

    add network to adminer

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,11 +1,13 @@
 import { authOptions } from "@/config/nextAuth";
+import { prisma } from "@/db/prismaClient";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  const session = await getServerSession(authOptions);
-  if (!session) {
-    return NextResponse.json({ error: "No session" }, { status: 401 });
-  }
-  return NextResponse.json(session, { status: 200 });
+  // const session = await getServerSession(authOptions);
+  // if (!session) {
+  //   return NextResponse.json({ error: "No session" }, { status: 401 });
+  // }
+  const users = await prisma.user.findMany();
+  return NextResponse.json(users, { status: 200 });
 }
