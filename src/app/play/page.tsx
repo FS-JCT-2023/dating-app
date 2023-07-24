52d72a2893a0eba4b77d6cb5241315bccc1ec3a6
Altbeg <shmuelbisberg.acc@gmail.com>
2023-07-24 20:38:41 +0300

    add migration

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -4,33 +4,70 @@ import { Button } from "@/components/ui/button";
 import { usePopper } from "@/providers/popper";
 
 export default function Playground() {
-  const {pop} = usePopper();
+  const { pop } = usePopper();
+
+  throw new Error("This is an error");
+  
   return (
     <>
-      <div className="">play</div>
-      <Button onClick={() => pop({
-        type: "success",
-        headline: "Success",
-        message: "This is a success message",
-      })}>Success</Button>
+      <div className="text-3xl font-semibold my-3 ml-2">play Page</div>
+      <div className="space-x-2 flex justify-center ">
+        <Button
+          onClick={() =>
+            pop({
+              type: "success",
+              headline: "Success",
+              message: "This is a success message",
+            })
+          }
+        >
+          Success
+        </Button>
 
-      <Button onClick={() => pop({
-        type: "error",
-        headline: "Error",
-        message: "This is an error message",
-      })}>Error</Button>
+        <Button
+          onClick={() =>
+            pop({
+              type: "error",
+              headline: "Error",
+              message: "This is an error message",
+            })
+          }
+        >
+          Error
+        </Button>
 
-      <Button onClick={() => pop({
-        type: "warning",
-        headline: "Warning",
-        message: "This is a warning message",
-      })}>Warning</Button>
+        <Button
+          onClick={() =>
+            pop({
+              type: "warning",
+              headline: "Warning",
+              message: "This is a warning message",
+            })
+          }
+        >
+          Warning
+        </Button>
 
-      <Button onClick={() => pop({
-        type: "info",
-        headline: "Info",
-        message: "This is an info message",
-      })}>Info</Button>
+        <Button
+          onClick={() =>
+            pop({
+              type: "info",
+              headline: "Info",
+              message: "This is an info message",
+            })
+          }
+        >
+          Info
+        </Button>
+
+        <Button
+          onClick={() => {
+            throw new Error("This is an error");
+          }}
+        >
+          Throw Error
+        </Button>
+      </div>
     </>
   );
 }
